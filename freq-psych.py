9529ffd2dae09f10897303831c332c9fb5ae164e
Darya Gorbunova <darya.a.gorbunova@yandex.ru>
2018-09-26 23:53:49 +0300

    Fish commit

--- freq-psych.py
+++ freq-psych.py
@@ -1,2 +1,48 @@
 #!/usr/bin/python
 print "abc"
+# reading and decoding input files
+textFile  = [""]
+psychFile = [""]
+
+# create dictionaries
+psychInf = {}
+infWords = {}
+comWords = {}
+markWords = {}
+numOfWordsInf = {}
+numOfWordsCom = {}
+numMarkWords = {}
+numOfWordsPsych = {}
+
+def readInfWords():
+	print "readInfWords()"
+	# text = open(","rt" ").read().lower()
+	# text normalization 
+	#   - lowercase
+	#   - remove all markup signs (time,paralinguistic signs with *)
+	#   - define informant|communicant
+	#   - compose full replica
+	#   - tokenize replicas by spaces and punctuation
+	#   - distribute words by dictionaries infWords and comWords 
+	#   - count them in numOfWordsInf and numOfWordsCom
+
+def sumNumOfWordsPsych():
+	print "sumNumOfWordsPsych()"
+	# assign a psychotype
+	# count number of words by psychotype in numOfWordsPsych
+
+def printResult():
+	print "printResult()"
+	# print psychInf  
+	# print infWords 
+	# print comWords 
+	# print markWords 
+	# print numOfWordsInf 
+	# print numOfWordsCom 
+	# print numMarkWords 
+	# print numOfWordsPsych 
+
+
+readInfWords()
+sumNumOfWordsPsych()
+printResult()
