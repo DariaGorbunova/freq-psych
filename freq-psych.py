6c15b72895f71ca6c9a6e61e53793efa95d1a077
Darya Gorbunova <darya.a.gorbunova@yandex.ru>
2018-09-27 17:45:11 +0300

    Re.fish commit

--- freq-psych.py
+++ freq-psych.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
-print "abc"
+# # -*- coding: utf8 -*-
+
+import re
+
 # reading and decoding input files
-textFile  = [""]
-psychFile = [""]
+textFile  = "/home/darya/work/lingvo_data/sample.txt"
+psychFile = "/home/darya/work/lingvo_data/PsyType.txt"
 
 # create dictionaries
 psychInf = {}
@@ -16,10 +19,28 @@ numOfWordsPsych = {}
 
 def readInfWords():
 	print "readInfWords()"
-	# text = open(","rt" ").read().lower()
+
+	textOpen = open(textFile,"rt")
+	text =  textOpen.read().decode("UTF-8").lower()
+
 	# text normalization 
 	#   - lowercase
 	#   - remove all markup signs (time,paralinguistic signs with *)
+	clearData = re.sub(u"/", "", text)
+	clearData = re.sub(u"ord[0-9a-zA-Zа-яА-Я-]*([^0-9a-zA-Zа-яА-Я-])[ 0-9]*", "", clearData)
+	clearData = re.sub(u"#", "", clearData)
+	clearData = re.sub(u"\*[а-яА-Я]+", "", clearData)
+	clearData = re.sub(u"/", "", clearData)
+	clearData = re.sub(u'[0-9]*:[0-9]+:[0-9]+\.[0-9]+', "", clearData)
+	clearData = re.sub(u"@", "", clearData)
+	clearData = re.sub(u"\?", "", clearData)
+	clearData = re.sub(u"\(", "", clearData)
+	clearData = re.sub(u"\)", "", clearData)
+	clearData = re.sub(u"\:", "", clearData)
+	clearData = re.sub(u"\.\.\.", "", clearData)
+	clearData = re.sub(u"\*.", "", clearData)
+	#clearData = re.sub(u"\s+"," ", clearData)
+	print clearData
 	#   - define informant|communicant
 	#   - compose full replica
 	#   - tokenize replicas by spaces and punctuation
